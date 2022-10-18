a23fb0c762388cb6b584acb6be2d2a3526feab6e
zerkani yanis <zrn3963a@ens.fsi>
2022-10-18 17:46:26 +0200

    fin assertions

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,40 +3,58 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+
 	}
-	
-	public String getNom() {
-		return nom;
-	}
-	public int getForce() {
-		return force;
-	}
-	
-	public void parler(String texte) {
-		System.out.println(prendreParole() + "<<" + texte + ">>");
-		
-	}
-	private String prendreParole() {
-		return "Le romain " + nom + " : ";
-		
-	}
-	
+
 	public void recevoirCoup(int forceCoup) {
+		assert isForcePositive();
+
+		int forceOrigine = force;
 		force -= forceCoup;
+
 		if (force > 0) {
 			parler("Aïe");
 		} else {
 			parler("J'abondonne...");
 		}
+		assert (force < forceOrigine);
 	}
+
+	private boolean isForcePositive() {
+		boolean forcePositive = false;
+		if (force > 0) {
+			forcePositive = true;
+		}
+
+		return forcePositive;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	public void parler(String texte) {
+		System.out.println(prendreParole() + "<<" + texte + ">>");
+
+	}
+
+	private String prendreParole() {
+		return "Le romain " + nom + " : ";
+
+	}
+
 	public static void main(String[] args) {
-		Romain cesar = new Romain("cesar", 3);
-		cesar.parler("Bonjour");
-		cesar.recevoirCoup(2);
-	
+		Romain minus = new Romain("Minus", 6);
+		minus.parler("Bonjour");
+		minus.recevoirCoup(3);
+
 	}
 }
