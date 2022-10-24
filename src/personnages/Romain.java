c0a8d91edce0fbb602d1e094ec88e2d85b83db2e
Yanis ZERKANI <yanis.yzr@gmail.com>
2022-10-24 09:11:42 +0200

    tp2 terminé

--- src/personnages/Romain.java
+++ src/personnages/Romain.java
@@ -3,13 +3,31 @@ package personnages;
 public class Romain {
 	private String nom;
 	private int force;
-	private Equipement[] equipement = new Equipement[2];
+	private Equipement[] equipements; 
 	private int nbEquipement = 0;
-	
+
 	public Romain(String nom, int force) {
 		this.nom = nom;
 		this.force = force;
+		this.equipements = new Equipement[2];
 		assert isForcePositive();
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public int getForce() {
+		return force;
+	}
+
+	private boolean isForcePositive() {
+		boolean forcePositive = false;
+		if (force > 0) {
+			forcePositive = true;
+		}
+
+		return forcePositive;
 	}
 
 	public void recevoirCoup(int forceCoup) {
@@ -26,23 +44,6 @@ public class Romain {
 		assert (force < forceOrigine);
 	}
 
-	private boolean isForcePositive() {
-		boolean forcePositive = false;
-		if (force > 0) {
-			forcePositive = true;
-		}
-
-		return forcePositive;
-	}
-
-	public String getNom() {
-		return nom;
-	}
-
-	public int getForce() {
-		return force;
-	}
-
 	public void parler(String texte) {
 		System.out.println(prendreParole() + "<<" + texte + ">>");
 
@@ -53,10 +54,49 @@ public class Romain {
 
 	}
 
+	public void sEquiper(Equipement equipement) {
+		switch (nbEquipement) {
+		case 0:
+			ajouterEquipement(equipement);
+			break;
+		case 1:
+
+			if (equipements[0] != equipement) {
+				ajouterEquipement(equipement);
+			} else {
+				System.out.println("Le soldat " + nom + " possède deja un " + equipement + "!");
+			}
+			break;
+		default:
+			System.out.println("Le soldat " + nom + " est déjà bien protégé !");
+			break;
+		}
+
+	}
+
+	private void ajouterEquipement(Equipement equipement) {
+		equipements[nbEquipement] = equipement;
+		System.out.println("Le soldat " + nom + " s'équipe avec un " + equipement + ".");
+		nbEquipement ++;
+	}
+
 	public static void main(String[] args) {
 		Romain minus = new Romain("Minus", 6);
 		minus.parler("Bonjour");
-		minus.recevoirCoup(3);
-		System.out.println(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.CASQUE);
+		minus.sEquiper(Equipement.BOUCLIER);
+		minus.sEquiper(Equipement.BOUCLIER);
 	}
 }
+
+
+
+
+
+
+
+
+
+
+
