0867c5f14e4e67d988b9f1e0ed9618bcb06c6ea5
Yanis ZERKANI <yanis.yzr@gmail.com>
2022-10-27 11:43:21 +0200

    sonarLint

--- src/personnages/Gaulois.java
+++ src/personnages/Gaulois.java
@@ -4,7 +4,9 @@ public class Gaulois {
 	
 	private String nom;
 	private int force;
+	private int nbTrophees;
 	private int effetPotion = 1;
+	private Equipement[] trophees = new Equipement[100];
 	
 	public Gaulois(String nom, int force) {
 		this.nom = nom;
@@ -22,14 +24,25 @@ public class Gaulois {
 	
 	
 	
+//	private String prendreParole() {
+//		return "Le gaulois " + nom + " : ";
+//	}
 	private String prendreParole() {
 		return "Le gaulois " + nom + " : ";
-	}
+		}
 	
+//	public void frapper(Romain romain) {
+//		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
+//		romain.recevoirCoup((force / 3) * effetPotion);
+//	}
 	public void frapper(Romain romain) {
-		System.out.println(nom + " envoie un grand coup dans la machoire de " + romain.getNom());
-		romain.recevoirCoup((force / 3) * effetPotion);
-	}
+		System.out.println(nom + " envoie un grand coup dans la mâchoire de " + romain.getNom());
+		Equipement[] trophees = romain.recevoirCoup((force / 3) * effetPotion);
+		for (int i = 0; trophees != null && i < trophees.length; i++, nbTrophees++) {
+		this.trophees[nbTrophees] = trophees[i];
+		}
+		}
+
 
 	public void boirePotion(int forcePotion) {
 		effetPotion = forcePotion;
